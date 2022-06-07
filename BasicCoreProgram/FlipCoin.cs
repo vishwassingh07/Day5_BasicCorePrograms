54eab2ca7da559253ba6d89040ba9ded43c829d0
Vishwas <baghel.vishwas0@gmail.com>
2022-06-07 11:51:52 +0530

    [Vishwas]add: Flip Coin Percentage Program Added

---
+++ BasicCoreProgram/FlipCoin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class FlipCoin
+    {
+       public void FlipPercent()
+        {
+            int headCount = 0, tailCount = 0;
+            Console.WriteLine("How many flips do you want ? ");
+            int flipNumber = Convert.ToInt32(Console.ReadLine());
+
+            while(flipNumber <= 0)
+            {
+                Console.WriteLine("Invalid input , please enter a valid number greater than 0");
+                flipNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < flipNumber; i++)
+            {
+                Random flip = new Random();
+                int option = flip.Next(0, 2);
+
+                if (option > headCount)
+                {
+                    headCount++;
+                }
+                else
+                {
+                    tailCount++;
+                }
+            }
+            Console.WriteLine("Number of heads : {0} ", headCount);
+            Console.WriteLine("Number of tails : {0} ", tailCount);
+            float headPercentage = (float)headCount * 100 / flipNumber;
+            float tailPercentage = (float)tailCount * 100 / flipNumber;
+            Console.WriteLine("Percentage of Head : {0} ", headPercentage);
+            Console.WriteLine("Percentage of Tail : {0} ", tailPercentage);
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Basice Core Programs....");
+            bool end = true;
+            Console.WriteLine("1. Flip Coin Percentage Problem\n2. End The Program");
+            while (end)
+            {
+                Console.WriteLine("Choose an option to execute...");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        FlipCoin flip = new FlipCoin();
+                        flip.FlipPercent();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option...");
+                        break;
+                }
+            }
+        }
+    }
+}
